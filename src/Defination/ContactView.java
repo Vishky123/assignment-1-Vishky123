2d9c3cdd97593ba0663468273a41d944ffc33633
Vishal Yadav <vishal.yadav_cs18@gla.ac.in>
2020-02-23 14:35:42 +0530

    Using java Collection class to sort the Contact name.

--- src/Defination/ContactView.java
+++ src/Defination/ContactView.java
@@ -4,6 +4,7 @@ import Adts.ContactViewAdt;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public class ContactView<E> implements ContactViewAdt {
     @Override
@@ -14,6 +15,11 @@ public class ContactView<E> implements ContactViewAdt {
         for (int i = 0; i < contacts.size(); i++) {
             ArrayList contact = new ArrayList((Collection) contacts.get(i));
             contactNames.add(contact.get(0));
+        }
+        Collections.sort(contactNames);
+        for (int i = 0; i < contactNames.size(); i++) {
+            System.out.println(contactNames.get(i).toString());
+
         }
 
 
