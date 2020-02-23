dfee74677e0179316809708203a3353303709c88
Vishal Yadav <vishal.yadav_cs18@gla.ac.in>
2020-02-23 13:58:32 +0530

    create Arraylist object to get data from DataStructure for searching.

--- src/Defination/ContactSearch.java
+++ src/Defination/ContactSearch.java
@@ -2,9 +2,12 @@ package Defination;
 
 import Adts.ContactSearchAdt;
 
+import java.util.ArrayList;
+
 public class ContactSearch<E> implements ContactSearchAdt<E> {
     @Override
     public void searchContact(String name) {
+        ArrayList contacts = new ArrayList(DataStructure.getConlist());
 
     }
 }
