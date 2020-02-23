21c96e86ca83ad7f2938f96a44916bea72694b5b
Vishal Yadav <vishal.yadav_cs18@gla.ac.in>
2020-02-23 12:42:46 +0530

    create a class for managing contact DataStructure.

--- src/Defination/ContactDelete.java
+++ src/Defination/ContactDelete.java
@@ -2,9 +2,13 @@ package Defination;
 
 import Adts.ContactDeleteAdt;
 
+import java.util.ArrayList;
+
 public class ContactDelete<E> implements ContactDeleteAdt<E> {
     @Override
     public boolean deleteContact(String name) {
-        return false;
+        ArrayList contact = new ArrayList();
+
+        return true;
     }
 }
