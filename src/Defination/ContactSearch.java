554a3f5a91b52ff8f327cd7298f17d419efd70c1
Vishal Yadav <vishal.yadav_cs18@gla.ac.in>
2020-02-23 14:26:10 +0530

    create the adt ContactViewAdt for viewing the all contact.

--- src/Defination/ContactSearch.java
+++ src/Defination/ContactSearch.java
@@ -3,12 +3,40 @@ package Defination;
 import Adts.ContactSearchAdt;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class ContactSearch<E> implements ContactSearchAdt<E> {
     @Override
     public void searchContact(String name) {
         ArrayList contacts = new ArrayList(DataStructure.getConlist());
         for (int i = 0; i < contacts.size(); i++) {
+            ArrayList contact = new ArrayList((Collection) contacts.get(i));
+            if ((contact.get(0) + "" + contact.get(1)).equals(name)) {
+
+            }
+        }
+    }
+
+    public void SearchContact(ArrayList contact) {
+        if (contact.size() >= 5) {
+            System.out.println("--------------------------------------------------------------");
+            System.out.println("First Name: " + contact.get(0));
+            System.out.println("Last Name: " + contact.get(1));
+
+            System.out.println("Contact Number(s):");
+            for (int i = 2; i < contact.size() - 1; i++) {
+                System.out.print(contact.get(i));
+            }
+            System.out.println();
+            System.out.println("Email:" + contact.get(contact.size() - 1));
+            System.out.println("--------------------------------------------------------------");
+        } else {
+            System.out.println("--------------------------------------------------------------");
+            System.out.println("First Name: " + contact.get(0));
+            System.out.println("Last Name: " + contact.get(1));
+            System.out.println("Contact Number:" + contact.get(2));
+            System.out.println("Email:" + contact.get(contact.size() - 1) + "\n");
+            System.out.println("--------------------------------------------------------------");
 
         }
     }
