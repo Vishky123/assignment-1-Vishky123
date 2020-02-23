9d2b7f96a0c4a34168bb156b2d0db4630d4ef24f
Vishal Yadav <vishal.yadav_cs18@gla.ac.in>
2020-02-23 13:01:22 +0530

    create adt ContactSearchAdt for searching the given contact in Data.

--- src/Defination/ContactDelete.java
+++ src/Defination/ContactDelete.java
@@ -3,12 +3,19 @@ package Defination;
 import Adts.ContactDeleteAdt;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class ContactDelete<E> implements ContactDeleteAdt<E> {
     @Override
     public boolean deleteContact(String name) {
-        ArrayList contact = new ArrayList(DataStructure.getConlist());
-
-        return true;
+        ArrayList contacts = new ArrayList(DataStructure.getConlist());
+        for (int i = 0; i < contacts.size(); i++) {
+            ArrayList contact = new ArrayList((Collection) contacts.get(i));
+            if ((contact.get(0) + "" + contact.get(1)).equals(name)) {
+                DataStructure.removeContact(i);
+                return true;
+            }
+        }
+        return false;
     }
 }
