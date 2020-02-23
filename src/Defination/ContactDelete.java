3d2cbc92978abc07597ed96ff5380b79cee90399
Vishal Yadav <vishal.yadav_cs18@gla.ac.in>
2020-02-23 12:39:23 +0530

    create a defination class for delete contact.

---
+++ src/Defination/ContactDelete.java
@@ -0,0 +1,10 @@
+package Defination;
+
+import Adts.ContactDeleteAdt;
+
+public class ContactDelete<E> implements ContactDeleteAdt<E> {
+    @Override
+    public boolean deleteContact(String name) {
+        return false;
+    }
+}
